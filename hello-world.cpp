53396ef4ca76a6c88774841d76558438f07b9888
Miguel Edward A. Rosal <miguelrosal12@gmail.com>
2025-02-10 16:39:55 +0800

    Initial commit

---
+++ hello-world.cpp
@@ -0,0 +1,42 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    int num1;
+    int num2;
+    int operation;
+    int ans = 0;
+
+    cout << "Hello World";
+
+    cout << "Enter first number: ";
+    cin >> num1;
+
+    cout << "Enter operation (1-addition, 2-subtraction, 3-division, 4-multiplication, 5-modulo): ";
+    cin >> operation;
+
+    cout << "Enter second number: ";
+    cin >> num2;
+
+    switch (operation)
+    {
+    case 1:
+        ans = num1 + num2;
+        break;
+    case 2:
+        ans = num1 - num2;
+        break;
+    case 3:
+        ans = num1 / num2;
+        break;
+    case 4:
+        ans = num1 * num2;
+        break;
+    default:
+        cout << "Invalid Operation";
+        break;
+    }
+
+    cout << "Result: " << ans;
+}
